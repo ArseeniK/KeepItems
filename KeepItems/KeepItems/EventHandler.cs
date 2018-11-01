50945b1563f8f02cc9e3b235209aec5466b4bd5c
ArseeniK <valentin7712@live.fr>
2018-11-01 20:21:00 +0100

    First version
    First version

---
+++ KeepItems/KeepItems/EventHandler.cs
@@ -0,0 +1,64 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using System.Collections.Generic;
+
+namespace Smod.TestPlugin
+{
+    class EventHandler : IEventHandlerSetRole, IEventHandlerCheckEscape
+    {
+        private Plugin plugin;
+
+        public EventHandler(Plugin plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public static Dictionary<int, List<int>> escapeItems = new Dictionary<int, List<int>>();
+
+        public void OnSetRole(PlayerSetRoleEvent ev)
+        {
+            if (ev.Role != Role.SCIENTIST && ev.Role != Role.CLASSD) { return; }
+            int playerId = ev.Player.PlayerId;
+            int count = ev.Items.Count;
+            Vector pos = ev.Player.GetPosition();
+            float x = pos.x; float y = pos.y; float z = pos.z;
+            y = y + 5; Vector itemPos = new Vector(x, y, z);
+            /*foreach (KeyValuePair<int, List<int>> kv in escapeItems)
+            {
+                plugin.Debug("[KeepItems] Items found for player " + kv.Key + " : " + kv.Value + "");
+            }*/
+            if (escapeItems.ContainsKey(ev.Player.PlayerId)) {
+                plugin.Debug("Escape Items found for " + ev.Player.Name + " (" + ev.Player.PlayerId + "). Have currently " + count + " items");
+                if (escapeItems.TryGetValue(ev.Player.PlayerId, out List<int> tmp))
+                {
+                    foreach (int number in tmp)
+                    {
+                        if (count >= 8) {
+                            PluginManager.Manager.Server.Map.SpawnItem((ItemType)number, itemPos, pos);
+                        }
+                        else { ev.Items.Add((ItemType)number); }
+                        count++;
+                    }
+                }
+                else { plugin.Debug("Failed to get values for player " + ev.Player.Name + " (" + ev.Player.PlayerId + ")"); }
+                escapeItems.Remove(ev.Player.PlayerId);
+            }
+        }
+        public void OnCheckEscape(PlayerCheckEscapeEvent ev)
+        {
+            List<int> tmp = new List<int>();
+            foreach (Item item in ev.Player.GetInventory())
+            {
+                tmp.Add((int)item.ItemType);
+                plugin.Debug("Added item " + (int)item.ItemType + " for " + item.ItemType);
+            }
+            if (tmp.Count > 0) { escapeItems.Add(ev.Player.PlayerId, tmp); }
+            foreach (KeyValuePair<int, List<int>> kv in escapeItems)
+            {
+                // plugin.Debug("INFO ITEMS 2: " + kv.Key + " : " + kv.Value + "");
+            }
+        }
+    }
+}
---
+++ KeepItems/KeepItems/KeepItems.cs
@@ -0,0 +1,37 @@
+﻿using Smod.TestPlugin;
+using Smod2;
+using Smod2.Attributes;
+using Smod2.EventHandlers;
+using Smod2.Events;
+
+namespace KeepItems
+{
+    [PluginDetails(
+        author = "ArseeniK",
+        name = "KeepItems",
+        description = "Allow you keep items when escape",
+        id = "arsk.keepitems",
+        version = "1.0",
+        SmodMajor = 3,
+        SmodMinor = 1,
+        SmodRevision = 20
+        )]
+    class KeepItems : Plugin
+    {
+        public override void OnDisable()
+        {
+        }
+
+        public override void OnEnable()
+        {
+            this.Info("Loaded");
+        }
+
+        public override void Register()
+        {
+            // Register Events
+            this.AddEventHandler(typeof(IEventHandlerSetRole), new EventHandler(this), Priority.Highest);
+            this.AddEventHandler(typeof(IEventHandlerCheckEscape), new EventHandler(this), Priority.Highest);
+        }
+    }
+}
